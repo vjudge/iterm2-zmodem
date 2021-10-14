c14f0b0fada7b511b5051bf01694b212f3f15a3a
vjudge <gradonday@gmail.com>
2021-10-14 22:06:10 +0800

    update recv

--- iterm2-recv-zmodem.sh
+++ iterm2-recv-zmodem.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
-# Author: Matt Mastracci (matthew@mastracci.com)
-# AppleScript from http://stackoverflow.com/questions/4309087/cancel-button-on-osascript-in-a-bash-script
-# licensed under cc-wiki with attribution required 
-# Remainder of script public domain
 
-#FILE=`osascript -e 'tell application "iTerm" to activate' -e 'tell application "iTerm" to set thefile to choose folder with prompt "Choose a folder to place received files in"' -e "do shell script (\"echo \"&(quoted form of POSIX path of thefile as Unicode text)&\"\")"`
-FILE="/Users/"`whoami`"/Downloads"
+
+osascript -e 'tell application "iTerm2" to version' > /dev/null 2>&1 && NAME=iTerm2 || NAME=iTerm
+if [[ $NAME = "iTerm" ]]; then
+	FILE=$(osascript -e 'tell application "iTerm" to activate' -e 'tell application "iTerm" to set thefile to choose folder with prompt "Choose a folder to place received files in"' -e "do shell script (\"echo \"&(quoted form of POSIX path of thefile as Unicode text)&\"\")")
+else
+	FILE=$(osascript -e 'tell application "iTerm2" to activate' -e 'tell application "iTerm2" to set thefile to choose folder with prompt "Choose a folder to place received files in"' -e "do shell script (\"echo \"&(quoted form of POSIX path of thefile as Unicode text)&\"\")")
+fi
+
 if [[ $FILE = "" ]]; then
 	echo Cancelled.
 	# Send ZModem cancel
 	echo -e \\x18\\x18\\x18\\x18\\x18
-	echo "# Cancelled transfer       "
-	/usr/local/bin/growlnotify -a /Applications/iTerm.app -n "iTerm" -m 'Cancelled.' -t "File transfer"
+	sleep 1
 	echo
+	echo \# Cancelled transfer
 else
-	echo $FILE
-	cd "${FILE}"
-	/usr/local/bin/growlnotify -a /Applications/iTerm.app -n "iTerm" -m 'Transfer started...' -t "File transfer"
-	/usr/local/bin/rz
-	/usr/local/bin/growlnotify -a /Applications/iTerm.app -n "iTerm" -m 'Transfer finished.' -t "File transfer"
-	echo "# Transfer finished        "
+	cd "$FILE"
+	/usr/local/bin/rz -E -e -b --bufsize 4096
+	sleep 1
 	echo
+	echo
+	echo \# Sent \-\> $FILE
 fi
